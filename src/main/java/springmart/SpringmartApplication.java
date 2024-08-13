82d768409cdd47d07804eb42f6a56f42c09c5a83
redig <luizredig@gmail.com>
2024-08-13 16:20:22 -0300

    chore: init Spring Boot project

---
+++ src/main/java/springmart/SpringmartApplication.java
@@ -0,0 +1,13 @@
+package springmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmartApplication.class, args);
+	}
+
+}
